4e7d9038fd5cec2d8dcfd06310197b540dfba00c
ymmc1117-mct <ymmc1117@gmail.com>
2026-04-30 16:13:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache-first for app shell, stale-while-revalidate for everything else
 // (so Google Fonts get cached on first online load and work offline after).
 
-const VERSION = 'sky-striker-v15';
+const VERSION = 'sky-striker-v18';
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 
